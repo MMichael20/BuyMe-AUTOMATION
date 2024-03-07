b1f199e6b57dd5bd1caf10bb98c449f23f12d673
MMichael20 <moshkinm@gmail.com>
2024-03-07 19:52:34 +0200

    BuyMe-AUTOMATION || Editing API Routes

--- practice.py
+++ practice.py
@@ -1,7 +1,9 @@
-import json
+import requests 
 
-with open("data.json", "r") as file:
-    my_settings = file.read()
-    
-data = json.loads(my_settings)
-print(data["headless"])
+url = "https://buyme.co.il/siteapi"
+def register():
+    payload = {" password":"Aa123456789","name":"Michael","username":"rasaf67031@gosarlar.com","confirm":"Aa123456789","termsAndConditions":"true","creation_source":"11"}
+    response = requests.request("POST" , url + "/register", data=payload)
+    print(response.json)
+
+register()
