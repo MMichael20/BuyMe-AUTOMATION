f4d9cde053bdeab656c49126285ef5dacda1e731
MMichael20 <63416021+MMichael20@users.noreply.github.com>
2025-04-08 13:36:00 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -33,27 +33,27 @@ class TestBuyMe(TestCase):
     def test1_wrong_validation(self):
         self.buyme_page.sign_up("Michael", "goodemail@gmail.com", "12345678", "87654321")
         self.buyme_page.check_password_error()
-        time.sleep(5)
+
         
     def test2_wrong_email(self):
         self.buyme_page.sign_up("Michael", "thisisnotanemail", "12345678", "12345678")
         self.buyme_page.check_email_error()
-        time.sleep(5)
+
         
     def test3_sign_right(self):
         self.buyme_page.sign_up("Michael", "gefolap418@flexvio.com", "Aa12345678", "Aa12345678")
         self.buyme_page.is_entered() #Sometimes there is Recaptcha so it might fail
-        time.sleep(5)
+
         
     def test4_log_wrong_passwrod(self):
         self.buyme_page.log_in("foxaj92846@taobudao.com", "1234")
         self.buyme_page.check_log_password_error()
-        time.sleep(5)    
+
     
     def test5_log_wrong_email(self):
         self.buyme_page.log_in("gefolap418@flexvio.com", "Aa12345678")
         self.buyme_page.check_log_email_error()
-        time.sleep(5)    
+
         
     def test6_buy_gift(self):
         self.buyme_page.log_in("xefaka4589@tsderp.com", "Aa12345678")
@@ -62,7 +62,6 @@ class TestBuyMe(TestCase):
         self.buyme_page.gift_type()
         self.buyme_page.enter_details("ישראל ישראלי", "מזל טוב ליום הולדתך")
         self.buyme_page.send_sms("+972541231234", "+9721231234", "ישראל ישראלי")
-        time.sleep(5)
 
     def tearDown(self) -> None:
         self.driver.quit()
